032bbb872a26aabd2adea5826c4bfd98da555890
Kaio Silva <106553278+JKaiosilva@users.noreply.github.com>
2023-03-11 21:43:12 -0400

    atualizações de um sábado a noite

--- guiaPress/database/database.js
+++ guiaPress/database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('guiapress', 'root', '123456', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-04:00'
 })
 
 
